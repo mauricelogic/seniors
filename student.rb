58b795d2dabb554c9a60a6ff129e319ac1089ec8
Maurice Womack <womackm@gmail.com>
2013-11-11 13:01:25 -0500

    Final commit

--- student.rb
+++ student.rb
@@ -1,28 +1,28 @@
 class Student
 
-	attr_reader :first_name, :last_name, :grade
-	def initialize(first_name, last_name, grade)
-		@first_name = first_name
-		@last_name = last_name
-		@grade = grade
-	end
+        attr_reader :first_name, :last_name, :grade
+        def initialize(first_name, last_name, grade)
+                @first_name = first_name
+                @last_name = last_name
+                @grade = grade
+        end
 
-	def senior?
-		grade == 12
-	end
-	
-	def junior?
-		grade == 11
-	end
-	
+        def senior?
+                grade == 12
+        end
+        
+        def junior?
+                grade == 11
+        end
+        
 
-	def to_s
-		"#{last_name}, #{first_name}"
-	end
+        def to_s
+                "#{last_name}, #{first_name}"
+        end
 end
 
 def seniors(students)
-	students.select { |student| student.senior? }
+        students.select { |student| student.senior? }
 end
 
 def juniors(students)
@@ -34,6 +34,12 @@ sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
 all_students = [fred, sarah, jack]
 
+puts "Seniors:"
 seniors(all_students).each do |student|
-	puts student
+        puts student
+end
+
+puts "Juniors:"
+juniors(all_students).each do |student|
+        puts student
 end
